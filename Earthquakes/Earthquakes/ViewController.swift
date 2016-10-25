379d16921eee751bce78b93194ae127b20f77dd3
RRiOSDev <rr@rogerrobles.com>
2016-10-25 18:52:44 -0400

    Create Xcode project.

---
+++ Earthquakes/Earthquakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Earthquakes
+//
+//  Created by Macintosh on 10/25/16.
+//  Copyright © 2016 RR Web. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
