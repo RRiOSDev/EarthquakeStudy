905413bd57b35dc316678be1fd04a3f1a12748e8
RRiOSDev <rr@rogerrobles.com>
2016-10-25 19:19:42 -0400

    Create earthquake model class

---
+++ Earthquakes/Earthquakes/Earthquake.swift
@@ -0,0 +1,22 @@
+//
+//  Earthquake.swift
+//  Earthquakes
+//
+//  Created by Macintosh on 10/25/16.
+//  Copyright © 2016 RR Web. All rights reserved.
+//
+
+import Foundation
+
+class Earthquake {
+    
+    let magnitude: Double = 0
+    let time: Date = Date()
+    let url: URL? = nil
+    let place: String = ""
+    let details: URL? = nil
+    let longitude: Double = 0
+    let latitude: Double = 0
+    let depth: Double = 0
+    
+}
