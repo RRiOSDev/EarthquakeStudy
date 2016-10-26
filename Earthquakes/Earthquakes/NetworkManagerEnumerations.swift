eac4bea620dd3639d24e40d43b888595de58f6b3
RRiOSDev <rr@rogerrobles.com>
2016-10-25 20:58:54 -0400

    Create the base for our networking layer

---
+++ Earthquakes/Earthquakes/NetworkManagerEnumerations.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManagerEnumerations.swift
+//  Earthquakes
+//
+//  Created by Macintosh on 10/25/16.
+//  Copyright © 2016 RR Web. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoint: String {
+    case all_day
+}
